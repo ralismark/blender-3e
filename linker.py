2957c4437c6e2a42ff1f2ece9154a1975d0c481b
ralismark <timmy.yao@outlook.com.au>
2020-08-12 22:38:48 +1000

    Validate course codes

--- linker.py
+++ linker.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 from base import fragment
 
 # pylint: disable=invalid-name
@@ -7,11 +9,17 @@ setup = fragment.Fragment()
 # pylint: enable=invalid-name
 
 COURSE_PREFIX = "https://www.handbook.unsw.edu.au/undergraduate/courses/2020/"
+COURSE_PATTERN = re.compile(r'[A-Za-z]{4}[0-9]{4}')
 
 @setup.command("course")
 async def handbook_course(ctx, *codes):
     """
     Link to UNSW courses from their code
     """
+    for code in codes:
+        if not COURSE_PATTERN.fullmatch(code):
+            await ctx.send("\u200b:x: not a valid course code", delete_after=10)
+            return
+
     codes = [f"{COURSE_PREFIX}{code.strip().upper()}" for code in codes]
     await ctx.send("\u200b:bookmark_tabs: " + "\n".join(codes))
